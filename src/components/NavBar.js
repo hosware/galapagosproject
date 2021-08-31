ad39efdf5a9c863fa5687f40939ed4bf32dab4c9
or <prietopazjosue@gmail.com>
2021-08-31 11:36:11 +0200

    css welcome page

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+  
+import React, { Component } from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends Component {
+
+    render() {
+        return (
+                    <nav>
+                        <ul>
+                            <li><Link to='/nosotros'> <h4>Nosotros</h4></Link></li>
+                            <li><Link to='/contactanos'> <h4>Contáctanos</h4></Link></li>
+                        </ul> 
+                    </nav>
+        )
+    }
+}
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import './WelcomePage.css';
 import { Link } from 'react-router-dom';
 
-// import background from '../images/background.gif';
+import blueboobies from '../images/blueboobies.png';
 
 export default class WelcomePage extends Component {
 
     render() {
         return (
-            <section>
-                <div className='introBox'>
+            <section className="introPage">
+                <div className='introPage'>
                     <div className='banner'>
-                        <img></img>
-                        <h1>Envios Piquero</h1>
-                        <h2>Exporta e importa 📦 a Galápagos 🐢 o cualquier envio 📨 </h2>
-                        <div>
-                          <button className = "signUpUnder"> <Link to="signup"> <span className= "hoverfun"> WhatsApp </span> </Link></button>
-                          <button className = "signUpUnder"> <Link to="signup"> <span className= "hoverfun">  Llámanos </span></Link></button>
+                        <img class= "imageFront" src={blueboobies}></img>
+                        <div class="subbanner">
+                        <h1>Envíos Piquero</h1>
+                        <h2>Envios 📦 a Galápagos 🐢 </h2>
+                        <div class="subsubbanner">
+                          <button className = "buttonFormat"> <Link to="signup"> <span className= "hoverfun"> WhatsApp </span> </Link></button>
+                          <button className = "buttonFormat"> <Link to="signup"> <span className= "hoverfun">  Llámanos </span></Link></button>
+                        </div>
                         </div>
                     </div>
                 </div>
